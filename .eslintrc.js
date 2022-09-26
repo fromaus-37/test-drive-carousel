48cc459f2162368d528cde2ca3d3cfb16bb9e002
fromaus-37 <naveed_ausaf@hotmail.com>
2022-09-26 03:30:16 +0100

    :construction: Starting work on CarouselButton

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,15 @@
 module.exports = {
-  extends: ["eslint:recommended"],
+  plugins: ['react'],
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
   parserOptions: {
     // this is necessary because
     // defult is ES5 which throws
     //errors on things like arrow functions
     ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
   //following block is necessary because otherwise
   //ESLInt throws error on `require` and `module` keyword
@@ -12,5 +17,10 @@ module.exports = {
   //CommonJS module system) but are not a feature of ES5 or ES6
   env: {
     node: true,
+  },
+  settings: {
+    react: {
+      version: '16.14.0',
+    },
   },
 };
